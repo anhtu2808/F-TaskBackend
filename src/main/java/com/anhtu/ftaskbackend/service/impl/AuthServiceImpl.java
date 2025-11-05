0a139cf91e5a1bf33657f24972d0c49a4d2d1e45
haocp <haocpse184107@fpt.edu.vn>
2025-11-05 19:28:56 +0700

    fix create user partner and customer

--- src/main/java/com/anhtu/ftaskbackend/service/impl/AuthServiceImpl.java
+++ src/main/java/com/anhtu/ftaskbackend/service/impl/AuthServiceImpl.java
@@ -67,10 +67,10 @@ public class AuthServiceImpl implements AuthService {
     public LoginResponse login(LoginRequest loginRequest) {
         User user = userRepository.findByPhone(loginRequest.getPhone())
                 .orElseThrow(() -> new AppException(ErrorCode.UserNotFoundByPhone));
-        if(!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
             throw new AppException(ErrorCode.WrongPassword);
         }
-        if(!user.getIsActive()){
+        if (!user.getIsActive()) {
             throw new AppException(ErrorCode.UNAUTHORIZED);
         }
         return LoginResponse.builder()
@@ -85,16 +85,16 @@ public class AuthServiceImpl implements AuthService {
 //                .orElseThrow(() -> new AppException(ErrorCode.UserNotFoundByPhone))
 //        )
 //            throw new AppException(ErrorCode.UserNotMatch);
-        if(!request.getOtp().equals("123456"))
+        if (!request.getOtp().equals("123456"))
             throw new AppException(ErrorCode.OtpIsInvalid);
         boolean isNewUser = true;
         User user = userRepository.findByPhone(request.getPhone()).orElse(null);
-        if(user != null){
+        if (user != null) {
             isNewUser = false;
             user.setIsActive(true);
             userRepository.save(user);
         }
-        if(isNewUser){
+        if (isNewUser) {
             user = User.builder()
                     .phone(request.getPhone())
                     .password(passwordEncoder.encode(request.getPhone()))  //password bây giờ là sđt để tránh lỗi
@@ -102,17 +102,25 @@ public class AuthServiceImpl implements AuthService {
                     .build();
             user.setRole(roleRepository.findByName(request.getRole())
                     .orElseThrow(() -> new AppException(ErrorCode.RoleNotFoundByName)));
-            userRepository.save(user);
             switch (request.getRole()) {
-                case "CUSTOMER" -> customerRepository.save(Customer.builder()
-                        .user(user)
-                        .build());
-                case "PARTNER" -> partnerRepository.save(Partner.builder()
-                        .user(user)
-                        .isAvailable(true)
+                case "CUSTOMER" -> {
+                    Customer customer = Customer.builder()
+                            .user(user)
+                            .build();
+                    user.setCustomer(customer);
+                    userRepository.save(user);
+                }
+                case "PARTNER" -> {
+                    Partner partner = Partner.builder()
+                            .user(user)
+                            .isAvailable(true)
 //                            .districtIdsJson()
-                        .build());
+                            .build();
+                    user.setPartner(partner);
+                    userRepository.save(user);
+                }
             }
+
         }
         return LoginResponse.builder()
                 .accessToken(generateToken(user))
@@ -130,22 +138,6 @@ public class AuthServiceImpl implements AuthService {
                     .map(Permission::getName)
                     .collect(Collectors.toList());
 
-            String role = user.getRole().getName();
-            String roleId = "";
-            Long value = 0L;
-            switch (role){
-                case "CUSTOMER" -> {
-                    roleId = "customerId";
-                    value = customerRepository.findByUser_Id(user.getId())
-                            .orElseThrow(() -> new AppException(ErrorCode.CustomerNotFound)).getId();
-                }
-                case "PARTNER" -> {
-                    roleId = "partnerId";
-                    value = partnerRepository.findByUser_Id(user.getId())
-                            .orElseThrow(() -> new AppException(ErrorCode.PartnerNotFound)).getId();
-                }
-            }
-
             JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
                     .subject(user.getUsername())
                     .issuer("ftask")
@@ -155,7 +147,6 @@ public class AuthServiceImpl implements AuthService {
                     .claim("customerId", user.getCustomer() != null ? user.getCustomer().getId() : null)
                     .claim("partnerId", user.getPartner() != null ? user.getPartner().getId() : null)
                     .claim("role", user.getRole().getName())
-                    .claim(roleId, value)
                     .claim("permissions", scopes)
                     .build();
 
